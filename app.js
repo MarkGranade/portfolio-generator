89b3361f92151dbbb4d8721c99582391b6202caf
Granade <mark.granade@gmail.com>
2021-10-12 13:26:45 -0400

    finished module 9 lesson 2

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+// Using assignment destructuring to assign elements of an array to variable names in a single expression
+const [name, github] = profileDataArgs;
 
-    console.log('================');
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+// first argument is file name that will be created
+// the second argument is the data thats being written: HTML string template
+// third argument is the callback function that will handle any errors as well as the success message
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
