4d98c18d28a9ec14dce4048b06c3b70bec9b85b7
Granade <mark.granade@gmail.com>
2021-10-12 15:35:36 -0400

    installed and checked that inquirer was working

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// **npm Packages**
+const inquirer = require('inquirer');
+// // **Core Library Modules
+// const fs = require('fs');
+// // **Personal Modules**
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(name, github);
 
-// Using assignment destructuring to assign elements of an array to variable names in a single expression
-const [name, github] = profileDataArgs;
+// // first argument is file name that will be created
+// // the second argument is the data thats being written: HTML string template
+// // third argument is the callback function that will handle any errors as well as the success message
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-// first argument is file name that will be created
-// the second argument is the data thats being written: HTML string template
-// third argument is the callback function that will handle any errors as well as the success message
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
